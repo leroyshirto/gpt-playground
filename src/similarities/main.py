5fcba47a9298067a14f40db54cca959d6fb6aafe
Leroy Shirto <leroy.shirto@fasthosts.com>
2023-11-13 21:08:00 +0000

    Add docstrings to generate_embeddings() and similarity() functions

--- src/similarities/main.py
+++ src/similarities/main.py
@@ -5,6 +5,16 @@ import numpy as np
 openai.api_key = environ.get('OPENAPI_KEY')
 
 def generate_embeddings(text, model='text-similarity-davinci-001'):
+    """
+    Generates embeddings for the given text using the specified OpenAI model.
+
+    Args:
+    text (str): The input text to generate embeddings for.
+    model (str): The OpenAI model to use for generating embeddings. Default is 'text-similarity-davinci-001'.
+
+    Returns:
+    numpy.ndarray: The embeddings generated for the input text.
+    """
     text = text.replace('\n', '')
     embedding=  openai.embeddings.create(
         input=[text],
@@ -13,7 +23,18 @@ def generate_embeddings(text, model='text-similarity-davinci-001'):
 
     return embedding.data[0].embedding
 
+
 def similarity(embedding_a, embedding_b):
+    """
+    Calculates the cosine similarity between two embeddings.
+
+    Args:
+    embedding_a (numpy.ndarray): The first embedding.
+    embedding_b (numpy.ndarray): The second embedding.
+
+    Returns:
+    float: The cosine similarity between the two embeddings.
+    """
     return np.dot(embedding_a, embedding_b)
 
 if __name__ == '__main__':
